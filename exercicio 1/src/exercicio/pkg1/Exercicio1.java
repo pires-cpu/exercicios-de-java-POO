7b0d66bb4849facafdd6e1cd25138809f218a92b
Aluno São Lucas <aluno.saolucas@ensino.ulbra.net>
2026-03-13 15:03:01 -0300

    Exercicio

--- exercicio 1/src/exercicio/pkg1/Exercicio1.java
+++ exercicio 1/src/exercicio/pkg1/Exercicio1.java
@@ -15,7 +15,14 @@ public class Exercicio1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+      
+        ProdutoTecnologia objproduto1 = new ProdutoTecnologia("Telefone", "Motorola", 3750.88, 21);
+        objproduto1.exibirProduto();
+        
+        System.out.println("__________________________________________");
+        
+        ProdutoTecnologia objproduto2 = new ProdutoTecnologia("Notebook", "Positivo", 2059.30, 20);
+        objproduto2.exibirProduto();
     }
     
 }
